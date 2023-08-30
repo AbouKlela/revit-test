7b97a571f5155130a8b612b70124b23075e26fd4
hamod <hamod@DESKTOP-RGFF2E8>
2023-08-30 22:31:17 +0300

    Add project files.

---
+++ revit test/Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;  
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace revit_test
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            //let user selecet the object 
+            var obj = uidoc.Selection.PickObject(ObjectType.Element);
+            // from refrence to eleid
+            ElementId elementId = obj.ElementId;
+            //acces the element
+            Element ele = doc.GetElement(elementId);
+           
+
+
+            TaskDialog.Show("element information", "Element Name: " + Environment.NewLine + ele.Name + Environment.NewLine + "element ID" + Environment.NewLine + ele.Id);
+
+            
+
+
+
+            //var dialog = new UserControl1(obj);
+            //dialog.ShowDialog();
+
+
+            return Result.Succeeded;
+
+
+
+        }
+       
+    }
+}
